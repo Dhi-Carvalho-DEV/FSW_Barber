ea841052ce6ac1055588b3788dea0de436184c6b
Dhi-Carvalho-DEV <dhi.carvalho.dev@gmail.com>
2024-08-12 10:18:31 -0300

    chore: add next auth setup

--- src/app/_components/sidebar-sheet.tsx
+++ src/app/_components/sidebar-sheet.tsx
@@ -1,10 +1,17 @@
 import Link from "next/link"
 import Image from "next/image"
-import { Avatar, AvatarImage } from "./ui/avatar"
 import { Button } from "./ui/button"
 import { SheetClose, SheetContent, SheetHeader, SheetTitle } from "./ui/sheet"
-import { CalendarIcon, HomeIcon, LogOutIcon } from "lucide-react"
+import { CalendarIcon, HomeIcon, LogInIcon, LogOutIcon } from "lucide-react"
 import { quickSearchOptions } from "../_constants/search"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog"
 
 const SidebarSheet = () => {
   return (
@@ -13,14 +20,40 @@ const SidebarSheet = () => {
         <SheetTitle className="text-left">Menu</SheetTitle>
       </SheetHeader>
 
-      <div className="flex items-center gap-3 border-b border-solid py-5">
-        <Avatar>
+      <div className="flex items-center justify-between gap-3 border-b border-solid py-5">
+        <h2 className="font-bold">Olá, faça seu login</h2>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button size="icon">
+              <LogInIcon />
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="w-[90%] rounded-md">
+            <DialogHeader>
+              <DialogTitle>Faça login na plataforma</DialogTitle>
+              <DialogDescription>
+                Conecte-se usando sua conta do Google
+              </DialogDescription>
+            </DialogHeader>
+
+            <Button variant="outline" className="gap-1 font-bold">
+              <Image
+                src="/google.svg"
+                width={18}
+                height={18}
+                alt="Fazer login com o Google"
+              />
+              Google
+            </Button>
+          </DialogContent>
+        </Dialog>
+        {/*<Avatar>
           <AvatarImage src="https://unsplash.com/pt-br/fotografias/um-homem-com-um-colar-no-pescoco-naS61tXjXG4" />
         </Avatar>
         <div>
           <p className="font-bold">Diogo Santana</p>
           <p className="text-xs">dhi.carvalho.dev@gmail.com</p>
-        </div>
+        </div> */}
       </div>
 
       <div className="flex flex-col gap-2 border-b border-solid py-5">
