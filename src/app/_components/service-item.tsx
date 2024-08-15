49db5292a63988f0744d434edd0cc79d3145849b
Dhi-Carvalho-DEV <dhi.carvalho.dev@gmail.com>
2024-08-15 18:14:46 -0300

    feat: ensure bookings are listed on bookings page

--- src/app/_components/service-item.tsx
+++ src/app/_components/service-item.tsx
@@ -242,7 +242,7 @@ const ServiceItem = ({ service, barbershop }: ServiceItemProps) => {
                           <div className="flex items-center justify-between">
                             <h2 className="text-sm text-gray-400">Data</h2>
                             <p className="text-sm">
-                              {format(selectedDay, "d 'de MMMM", {
+                              {format(selectedDay, "d 'de' MMMM", {
                                 locale: ptBR,
                               })}
                             </p>
